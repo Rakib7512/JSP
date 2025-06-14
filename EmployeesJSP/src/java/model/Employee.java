dacec68c906de040c75d2f42edf8ca26a638b4a1
Rakib7512 <mdrakibislam7512@gmail.com>
2025-06-15 01:36:28 +0600

    add
    add

---
+++ EmployeesJSP/src/java/model/Employee.java
@@ -0,0 +1,60 @@
+
+package model;
+
+public class Employee {
+    int id;
+    String name;
+    String designation;
+    float salary;
+
+    public Employee() {
+    }
+
+    public Employee(int id, String name, String designation, float salary) {
+        this.id = id;
+        this.name = name;
+        this.designation = designation;
+        this.salary = salary;
+    }
+
+    public Employee(String name, String designation, float salary) {
+        this.name = name;
+        this.designation = designation;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+    
+    
+    
+}
